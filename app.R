54c90bbfa357081af4abe78d6087274d7552b2a4
Ibuki-Fuuko <yichuifeng0@gmail.com>
2026-04-29 13:53:02 -0500

    Update app.R

--- app.R
+++ app.R
@@ -2,7 +2,7 @@ library(shiny)
 library(plotly)
 library(dplyr)
 
-pd1 <- readRDS("pd1.rds")
+pd1 <- readRDS("/srv/shiny-server/pd1.rds")
 officer_counts <- pd1 |> dplyr::count(Officer_Name, sort = TRUE)
 ecdf_fn <- ecdf(officer_counts$n)
 
